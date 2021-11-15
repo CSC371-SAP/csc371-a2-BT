249e765dcf00d74372cf829752b27ed66d9dd9ff
Sakdipat Ontoum <sakdipat3536@gmail.com>
2021-11-15 09:54:35 +0700

    [ADD]

--- BT-Stream/src/com/example/btstream/BTStream.java
+++ BT-Stream/src/com/example/btstream/BTStream.java
@@ -1,22 +1,85 @@
 package com.example.btstream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 2, jvmArgs = {"-Xms2G", "-Xmx2G"})
+@Warmup(iterations = 3)
+@Measurement(iterations = 8)
 
 public class BTStream {
-    public static void main(String[] args) throws IOException {
-        Path path = Paths.get("dataset/5000 BT Records.csv");
-        if (Files.exists(path)) {
-            try {
-                Stream<String> stream = Files.lines(path);
-                stream.forEach(System.out::println);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("file does not exist");
+
+    @Param({"10000000"})
+    private int N;
+
+    private List<String> DATA_FOR_TESTING;
+
+    public static void main(String[] args) throws RunnerException {
+
+        Options opt = new OptionsBuilder()
+                .include(BTStream.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    @Setup
+    public void setup() {
+        DATA_FOR_TESTING = createData();
+    }
+
+    @Benchmark
+    public void loopFor(Blackhole bh) {
+        for (int i = 0; i < DATA_FOR_TESTING.size(); i++) {
+            String s = DATA_FOR_TESTING.get(i); //take out n consume, fair with foreach
+            bh.consume(s);
         }
+    }
+
+    @Benchmark
+    public void loopWhile(Blackhole bh) {
+        int i = 0;
+        while (i < DATA_FOR_TESTING.size()) {
+            String s = DATA_FOR_TESTING.get(i);
+            bh.consume(s);
+            i++;
+        }
+    }
+
+    @Benchmark
+    public void loopForEach(Blackhole bh) {
+        for (String s : DATA_FOR_TESTING) {
+            bh.consume(s);
+        }
+    }
+
+    @Benchmark
+    public void loopIterator(Blackhole bh) {
+        Iterator<String> iterator = DATA_FOR_TESTING.iterator();
+        while (iterator.hasNext()) {
+            String s = iterator.next();
+            bh.consume(s);
+        }
+    }
+
+    private List<String> createData() {
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            data.add("Number : " + i);
+        }
+        return data;
     }
 }
